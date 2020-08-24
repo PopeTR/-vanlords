997f36fc67631cb108e08d8fc2b62eacc06829cd
Tom Pope <popetr90@gmail.com>
2020-08-24 17:04:44 +0100

    fixing syntax issues

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
          has_many :vans, :reviews, :bookings
          validates :name, presence: true
-         validates :phone_number, presence: true,
+         validates :phone_number, presence: true
          validates :phone_number, uniqueness: true
          validates :name, uniqueness: true
 end
