820161deee70877ebccf14a6d48001a2ab6ae26d
Tom Pope <popetr90@gmail.com>
2020-08-24 17:25:23 +0100

    syntax change

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-         # has_many :vans, :reviews, :bookings
+         has_many :vans
+         has_many :reviews
+         has_many :bookings
          validates :name, presence: true
          validates :phone_number, presence: true
          validates :phone_number, uniqueness: true
