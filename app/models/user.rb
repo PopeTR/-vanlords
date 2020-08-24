ab48b9314dd13654c6cdb58a5197b32c0e274a40
Thijs Hakkenberg <thijshakkenberg@hotmail.com>
2020-08-24 16:18:36 +0200

    Created User model

---
+++ app/models/user.rb
@@ -0,0 +1,11 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+         has_many :vans, :reviews, :bookings
+         validates :name, presence: true
+         validates :phone_number, presence: true,
+         validates :phone_number, uniqueness: true
+         validates :name, uniqueness: true
+end
