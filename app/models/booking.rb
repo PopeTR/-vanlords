3246a2a966f60d2aa13a7e4c339245cf23639fc4
Tom Pope <popetr90@gmail.com>
2020-08-25 12:15:18 +0100

    tests

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,7 +2,7 @@ class Booking < ApplicationRecord
 	belongs_to :users
 	belongs_to :vans
 	validates :start_date, presence: true
-	validates :end_date, presence: :true
-	validates :total_price, presence: :true
-	validates :booking_status, presence: :true
+	validates :end_date, presence: true
+	validates :total_price, presence: true
+	validates :booking_status, presence: true
 end
