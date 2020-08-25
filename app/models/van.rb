9a5556424a0cf2c6b63e460569f06bb88953840a
Tom Pope <popetr90@gmail.com>
2020-08-25 07:45:39 +0100

    checking validations

--- app/models/van.rb
+++ app/models/van.rb
@@ -1,8 +1,8 @@
 class Van < ApplicationRecord
 	belongs_to :user
 	has_many :reviews, through: :bookings
-	# validates :availability, default: 1
-	# validates :description, presence: true
-	# validates :title, presence: true
-	# validates :price_per_night, presence: true
+	validates :availability, default: 1
+	validates :description, presence: true
+	validates :title, presence: true
+	validates :price_per_night, presence: true
 end
