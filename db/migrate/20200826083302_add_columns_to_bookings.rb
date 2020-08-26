10267495f2816f06268dff563635ab7d2d87f133
Tom Pope <popetr90@gmail.com>
2020-08-26 09:49:21 +0100

    fixing 1 issue

--- db/migrate/20200826083302_add_columns_to_bookings.rb
+++ db/migrate/20200826083302_add_columns_to_bookings.rb
@@ -1,6 +1,6 @@
 class AddColumnsToBookings < ActiveRecord::Migration[6.0]
   def change
-    add_reference :bookings, van:, foreign_key: true
-    add_reference :bookings, user:, foreign_key: true
+    add_reference :bookings, :van, foreign_key: true
+    add_reference :bookings, :user, foreign_key: true
   end
 end
