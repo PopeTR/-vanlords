f2c3faa222f7eca72cb74815f1deb33298f75942
Tom Pope <popetr90@gmail.com>
2020-08-27 16:34:15 +0100

    fixed conflicts

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,28 +1,28 @@
 class ReviewsController < ApplicationController
   before_action :set_review, only: [:show]
-
   def show
   end
-
   def new
     @review = Review.new
+    if params[:user_id].present?
+      @user = User.find(params[:user_id])
+    else params[:van_id].present?
+      @van = Van.find(params[:van_id])
+    end
   end
-
   def create
-    @review = Review.new(review_params)
+    @review = Review.new(params[:review].permit(:user_id))
+    @review.description = some input
     if @review.save
-      redirect_to review_path(@review)
+      redirect_to booking_path()
     else
       render :new
     end
   end
-
   private
-
   def set_review
     @review = Review.find(params[:id])
   end
-
   def review_params
     params[:review].permit(:rating, :description)
   end
