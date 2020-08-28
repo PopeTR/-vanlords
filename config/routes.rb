93b536045d60f9e31c18bcca86c421f2044c54d0
Florian Bucher <bucher.fl1@gmail.com>
2020-08-28 12:12:12 +0200

    Added links to profile page

--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,7 @@ Rails.application.routes.draw do
  get 'booking/:id', to: 'accounts#booking', as: :view_booking
  post 'booking', to: 'accounts#create_booking', as: :post_booking
  get 'booking', to: 'accounts#booking'
-  get 'guests', to: 'accounts#guests'
+ get 'guests', to: 'accounts#guests'
  get 'guest', to: 'accounts#guest'
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
