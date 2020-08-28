4b405efbdf6db952016d778a35aa9b4cc923aab7
Tom Pope <popetr90@gmail.com>
2020-08-28 14:27:20 +0100

    fixing modal login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,10 @@
 class ApplicationController < ActionController::Base
 # >>maybe this is needed to connect the dashboard/account page after signing in
-# def after_sign_in_path_for(resource)
-#   app_account_index_path
-# end
+  def after_sign_in_path_for(resource)
+    if request.referer == "http://localhost:3000/users/sign_in"
+      ""
+    else
+      request.referer
+    end
+  end
 end
